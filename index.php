40b4a3b51ccfeb5681bd6aac46e957f067c5c237
doomspork <iamdoomspork@gmail.com>
2011-08-28 09:20:31 -0400

    Cleaned up index, changed default Lumberjack setting to WARNING

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 * I'm not really excited about this file.  I'm not going to keep it around either
 * but for the time being, it'll be the easiest single point for the .htaccess file
 */
-
 error_reporting(E_ALL);//E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 define('DS', DIRECTORY_SEPARATOR);
@@ -22,9 +21,7 @@ require_once CORE_PATH . DS . 'dispatcher.php';
 require_once CORE_PATH . DS . 'lumberjack.php';
 
 $lumberjack = LumberJack::instance(new BasicOutput());
-$lumberjack->setReportingLevel(LumberJack::DEBUG);
-
-//RouteRegistry::instance(new FlatFileStorage());
+$lumberjack->setReportingLevel(LumberJack::WARNING);
 
 $dispatcher = new Dispatcher();
 $dispatcher->dispatch();
